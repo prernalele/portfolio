0038f6ec202898097523caa94a7861291adc0aff
Prerna Lele <prernalele@gmail.com>
2025-03-08 16:14:11 -0500

    feat: adds fontawesome

--- src/App.jsx
+++ src/App.jsx
@@ -14,31 +14,35 @@ function App() {
       title: "PlatePal",
       image: PlatePal,
       link: "https://platepal2.netlify.app/",
+      github: "https://github.com/chingu-voyages/V53-tier2-team-25",
     },
     {
       key: 2,
       title: "Advice generator",
       image: adviceGenerator,
       link: "https://prernalele.github.io/adviceGenerator/",
+      github: "https://github.com/prernalele/adviceGenerator",
     },
     {
       key: 3,
       title: "Where in the world?",
       image: WhereInTheWorld,
       link: "https://prernalele.github.io/countries-api-main/",
+      github: "https://github.com/prernalele/countries-api-main",
     },
     {
       key: 4,
       title: "Newsletter sign up",
       image: NewsletterSignup,
       link: "https://prernalele.github.io/code-challenge-newsletter-sign-up/",
+      github: "https://github.com/prernalele/code-challenge-newsletter-sign-up",
     },
-    {
-      key: 5,
-      title: "Grid Lights Game",
-      image: NewsletterSignup,
-      link: "https://prernalele.github.io/countries-api-main/",
-    },
+    // {
+    //   key: 5,
+    //   title: "Grid Lights Game",
+    //   image: NewsletterSignup,
+    //   link: "https://prernalele.github.io/countries-api-main/",
+    // },
   ];
   return (
     <main className=" max-h-full max-w-full flex flex-col gap-6 font-mono text-opacity-90 justify-center">
@@ -46,22 +50,28 @@ function App() {
         Prerna Lele
       </section>
       <section className="flex flex-col gap-2">
-        <h3 className="text-xl font-bold">About Me</h3>
+        <h3 className="text-3xl lg:text-xl font-bold">About Me</h3>
         <p className="text-sm">
           Welcome to my portfolio! I am Prerna Lele, a Frontend developer with 4
           years of relevant experience and a decade of overall industry
           experience. I love turning Figma designs to user friendly, responsive
-          interactive screens for users. I have also been a Quality Professional
-          and has led quality efforts in my previous experiences
+          interactive screens for users. I have also been a Quality Assurance
+          Professional and has led quality efforts in my previous experiences
         </p>
       </section>
       <section className="flex flex-col gap-4">
-        <h3 className="text-xl font-bold">Projects</h3>
+        <h3 className="text-3xl lg:text-xl font-bold">Projects</h3>
         <div className="flex flex-col lg:flex-row gap-10  lg:flex-wrap justify-center items-center">
           {projectsCollection.map((project) => {
             const { key, title, image, link } = project;
             return (
-              <Project key={key} title={title} image={image} link={link} />
+              <Project
+                key={key}
+                title={title}
+                image={image}
+                link={link}
+                github={github}
+              />
             );
           })}
         </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faPersonChalkboard,
+} from "@fortawesome/free-brands-svg-icons";
 
-const Project = ({ key, title, image, link }) => {
+const Project = ({ key, title, image, link, github }) => {
+  const openGithubRepo = () => {
+    window.open(github);
+  };
+  const openDemoLink = () => {
+    window.open(link);
+  };
   return (
     <div className="flex flex-col w-1/3 gap-y-2 ">
       <img src={image} className="rounded-md" />
-      <a href={link}>{title} </a>
+      <div className="flex flex-row justify-around">
+        <button
+          onClick={openGithubRepo}
+          className="bg-red-300 hover:bg-red-400 text-black opacity-90 font-bold py-2 px-4 rounded"
+        >
+          <FontAwesomeIcon icon={faGithub} />
+        </button>
+        <button
+          onClick={openDemoLink}
+          class="bg-red-300 hover:bg-red-400  text-black opacity-90 font-bold py-2 px-4 rounded"
+        >
+          <FontAwesomeIcon icon={faPersonChalkboard} />
+        </button>
+      </div>
     </div>
   );
 };
